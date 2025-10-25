8d1b1f286fef3683411c442050902109d37f8e8f
James Vongphasouk <james.vongpha@gmail.com>
2025-10-24 22:46:45 -0600

    feat: complete calc

--- scripts.js
+++ scripts.js
@@ -1,26 +1,34 @@
-let a = 0;
-let b = 0;
-let op = "";
+let a = "";
+let b = "";
+let op = null;
 let clearDisplayFlag = false;
 const display = document.querySelector("#display");
 
+function roundToTwoDecimalPlaces(number) {
+	return Math.round(number * 100) / 100;
+}
 function add(a, b) {
-	return a + b;
+	return roundToTwoDecimalPlaces(a + b);
 }
 
 function subtract(a, b) {
-	return a - b;
+	return roundToTwoDecimalPlaces(a - b);
 }
 
 function multiply(a, b) {
-	return a * b;
+	return roundToTwoDecimalPlaces(a * b);
 }
 
 function divide(a, b) {
-	return a / b;
+	if (b === 0) {
+		return "Error: Divide by 0";
+	}
+	return roundToTwoDecimalPlaces(a / b);
 }
 
 function operate(a, operand, b) {
+	a = Number(a);
+	b = Number(b);
 	switch (operand) {
 		case "+":
 			return add(a, b);
@@ -34,12 +42,13 @@ function operate(a, operand, b) {
 }
 
 function addDigitToDisplay(e) {
-	const datasetNum = parseInt(e.target.dataset.num);
+	const datasetNum = e.target.dataset.num;
 
 	if (clearDisplayFlag) {
-		display.textContent = datasetNum;
+		display.textContent = datasetNum; // replace text content
+		clearDisplayFlag = false;
 	} else {
-		display.textContent += datasetNum;
+		display.textContent += datasetNum; //append to the current text content
 	}
 }
 
@@ -47,7 +56,46 @@ const numberBtns = document.querySelectorAll("#numpad > button");
 
 numberBtns.forEach((btn) => {
 	btn.addEventListener("click", addDigitToDisplay);
-})
+});
+
+function calculate(e) {
+	if (e.target.className === "operator") {
+		if (op && a != "" && !clearDisplayFlag) {
+			// if there's a previous operation pending, calculate it first
+			b = parseInt(display.textContent);
+			const result = operate(a, op, b);
+			display.textContent = result;
+			a = result.toString();
+		} else {
+			a = display.textContent;
+		}
+
+		op = e.target.dataset.op;
+		clearDisplayFlag = true;
+
+	} else if (e.target.id === "equals" && op) { // do the equals operation
+		if (a != "" && !clearDisplayFlag) {
+			b = parseInt(display.textContent);
+			const result = operate(a, op, b);
+			display.textContent = result;
+			a = result.toString();
+			op = null;
+			clearDisplayFlag = true;
+		}
+	} else if (e.target.id === "clear") { // clear everything
+		a = "";
+		b = "";
+		op = null;
+		clearDisplayFlag = false;
+		display.textContent = "";
+	}
+}
 
 // pressing an operator button (+, -, /, should set the clearDisplayFlag to true)
-//
+const operatorBtns = document.querySelectorAll("#operators > button");
+
+operatorBtns.forEach((btn) => {
+
+	btn.addEventListener("click", calculate);
+});
+
